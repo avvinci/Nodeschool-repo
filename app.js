7630c91867982eef9c26f9da6d8d5c494bc9083e
avvinci <vinciabhinav7@gmail.com>
2019-07-08 10:15:46 +0530

    csv write working

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var electron = require('electron')
 
 electron.app.on('ready', function(){
-    var mainWindow = new electron.BrowserWindow({width:600, height:800})
+    var mainWindow = new electron.BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width:800,
+        height:700})
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 })
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 function saveFn(){
     
-let q = document.getElementById('quoteTA') ; 
-let a = document.getElementById('authorTA') ; 
-console.log(q.innerHTML); 
-console.log(a.innerHTML); 
+    let q = document.getElementById('quoteTA') ; 
+    let a = document.getElementById('authorTA') ; 
+    console.log(q.value); 
+    let content  = q.value
+    console.log(a); 
+    var fs = require('fs');
+    try { fs.writeFileSync('myfile.txt', content, 'utf-8'); }
+    catch(e) { alert('Failed to save the file !'); }
+
+    const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+    const csvWriter = createCsvWriter({
+        path: 'quotes.csv',
+        header: [
+            {id: 'quote', title: 'QUOTE'},
+            {id: 'author', title: 'AUTHOR'}
+        ]
+    });
+    
+    const records = [
+        {quote: 'A journey begins',  author: 'avvinci'},
+        {quote: 'Perfect is the enemy of good',  author: 'Anne'},
+
+    ];
+    
+    csvWriter.writeRecords(records)       // returns a promise
+        .then(() => {
+            console.log('...Done');
+        });
+        
 
 
 }
-
-
 
 let b = document.getElementById('addButton') ; 
 
