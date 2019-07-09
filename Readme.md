d615ac1ccb9c6f40b8370c0ca0d999e02c61b270
avvinci <vinciabhinav7@gmail.com>
2019-07-09 10:45:15 +0530

    working on tray icon

--- Readme.md
+++ Readme.md
@@ -5,4 +5,7 @@
 
 #### Todo
 1. Package and upload to npm library
-
+2. Add tray input   
+3. add toast
+4. Improve design
+5. 
--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var electron = require('electron')
+const { app, Menu, Tray } = require('electron')
+let tray = null
+let window = undefined
 
 electron.app.on('ready', function(){
     var mainWindow = new electron.BrowserWindow({
@@ -8,4 +11,49 @@ electron.app.on('ready', function(){
         width:1000,
         height:700})
     mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+    createTray() ; 
+    createWindow();
+
 })
+
+const createWindow = () => {
+    window = new electron.BrowserWindow({
+        width: 320,
+        height: 450,
+        show: false,
+        frame: false,
+        fullscreenable: false,
+        resizable: false,
+        transparent: true,
+    });
+    window.loadURL(`file://${__dirname}/tray.html`)
+    
+}
+
+
+const createTray = () => {
+    tray = new Tray('./Quotes.ico')
+    tray.setToolTip('Quotes App.')
+    tray.on('click', function (event) {
+        toggleWindow();
+    })
+}
+const toggleWindow = () => {
+    window.isVisible() ? window.hide() : showWindow();
+}
+const showWindow = () => {
+    const position = getWindowPosition();
+    window.setPosition(position.x, position.y, false);
+    window.show();
+}
+const getWindowPosition = () => {
+    const windowBounds = window.getBounds();
+    const trayBounds = tray.getBounds();
+    
+    // Center window horizontally below the tray icon
+    const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+    // Position window 4 pixels vertically below the tray icon
+    const y = Math.round(trayBounds.y + trayBounds.height + 4)
+    return {x: x, y: y}
+}
