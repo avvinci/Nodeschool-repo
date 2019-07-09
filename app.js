72e74d698c38afcf56d76511c997c979a31069cf
avvinci <vinciabhinav7@gmail.com>
2019-07-09 12:05:00 +0530

    tray module added

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var electron = require('electron')
-const { app, Menu, Tray } = require('electron')
-let tray = null
-let window = undefined
-
+const trayWindow = require("electron-tray-window");
 electron.app.on('ready', function(){
     var mainWindow = new electron.BrowserWindow({
         webPreferences: {
@@ -12,48 +9,15 @@ electron.app.on('ready', function(){
         height:700})
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-    createTray() ; 
-    createWindow();
+    trayWindow.setOptions({
+        trayIconPath: "./Quotes.ico",
+        windowUrl: `file://${__dirname}/tray.html`
+      });
 
-})
-
-const createWindow = () => {
-    window = new electron.BrowserWindow({
-        width: 320,
-        height: 450,
-        show: false,
-        frame: false,
-        fullscreenable: false,
-        resizable: false,
-        transparent: true,
+    trayWindow.setWindowSize({
+        width    : 400,    //optional
+        height   : 400,   //optional
+        margin_x : 10,  //optional
+        margin_y : 10   //optional
     });
-    window.loadURL(`file://${__dirname}/tray.html`)
-    
-}
-
-
-const createTray = () => {
-    tray = new Tray('./Quotes.ico')
-    tray.setToolTip('Quotes App.')
-    tray.on('click', function (event) {
-        toggleWindow();
-    })
-}
-const toggleWindow = () => {
-    window.isVisible() ? window.hide() : showWindow();
-}
-const showWindow = () => {
-    const position = getWindowPosition();
-    window.setPosition(position.x, position.y, false);
-    window.show();
-}
-const getWindowPosition = () => {
-    const windowBounds = window.getBounds();
-    const trayBounds = tray.getBounds();
-    
-    // Center window horizontally below the tray icon
-    const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
-    // Position window 4 pixels vertically below the tray icon
-    const y = Math.round(trayBounds.y + trayBounds.height + 4)
-    return {x: x, y: y}
-}
+})
