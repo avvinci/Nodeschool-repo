37d925a064adddb77ab703df6a91d40ef5e97582
avvinci <vinciabhinav7@gmail.com>
2019-07-12 13:41:30 +0530

    packaging configuration done

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var electron = require("electron");
 const trayWindow = require("electron-tray-window");
 electron.app.on("ready", function() {
-  // var mainWindow = new electron.BrowserWindow({
-  //   webPreferences: {
-  //     nodeIntegration: true
-  //   },
-  //   width: 800,
-  //   height: 600
-  // });
-  // mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   var mytrayWindow = new electron.BrowserWindow({
     webPreferences: {
@@ -27,9 +19,9 @@ electron.app.on("ready", function() {
   });
 
   trayWindow.setWindowSize({
-    width: 400, //optional
-    height: 400, //optional
-    margin_x: 10, //optional
-    margin_y: 10 //optional
+    width: 400,
+    height: 400, 
+    margin_x: 10,
+    margin_y: 10 
   });
 });
