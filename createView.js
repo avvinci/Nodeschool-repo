9ee2647a43f2409c40c2281769e48df607af358f
avvinci <vinciabhinav7@gmail.com>
2019-07-23 08:19:51 +0530

    created viewjs

---
+++ createView.js
@@ -0,0 +1,42 @@
+let viewButton = getElem("viewButton");
+if (viewButton) viewButton.addEventListener("click", viewFn);
+let addBox = getElem("addBox");
+
+function createViewBox() {
+    let box = document.createElement("div");
+    box.setAttribute("id", "viewBox");
+    box.setAttribute("class", "list-group");    
+    let addButton = document.createElement("Button");
+    addButton.innerHTML = "Submit New Quote";
+    addButton.setAttribute("class", "btn btn-success btn-block mb-5 mt-5");
+    addButton.style.width = "60%";
+    addButton.style.marginLeft = "20%";
+  
+    box.appendChild(addButton);
+    addButton.onclick = () => {
+      addBox.style.display = "block";
+      getElem("viewBox").remove();
+    };
+  
+    return box;
+  }
+
+function viewFn() {
+    addBox.style.display = "none";
+    let box = createViewBox();
+    ipcRenderer.send("needViewData", null);
+  
+    ipcRenderer.on("viewData", (event, jsonObj) => {
+      jsonObj.forEach(elem => {
+        var newlabel = document.createElement("Label");
+        newlabel.innerHTML = elem.QUOTE;
+        newlabel.style.width = "80%";
+        newlabel.style.marginLeft = "10%";
+  
+        newlabel.setAttribute("class", "list-group-item");
+        box.appendChild(newlabel);
+      });
+    });
+    let bxc = getElem("boxContainer");
+    bxc.appendChild(box);
+  }
--- index.js
+++ index.js
@@ -6,35 +6,12 @@ function getElem(id) {
 
 let quoteTA = getElem("quoteTA");
 let authorTA = getElem("authorTA");
-let addBox = getElem("addBox");
-let viewButton = getElem("viewButton");
 let mainAddButton = getElem("addButton");
 mainAddButton.addEventListener("click", saveFn);
 
-if (viewButton) viewButton.addEventListener("click", viewFn);
-
-window.onload = function() {
-  document.getElementById("loading").style.display = "none";
-};
-
-function createViewBox() {
-  let box = document.createElement("div");
-  box.setAttribute("id", "viewBox");
-  box.setAttribute("class", "list-group");
-  let addButton = document.createElement("Button");
-  addButton.innerHTML = "Submit New Quote";
-  addButton.setAttribute("class", "btn btn-success btn-block mb-5 mt-5");
-  addButton.style.width = "60%";
-  addButton.style.marginLeft = "20%";
-
-  box.appendChild(addButton);
-  addButton.onclick = () => {
-    addBox.style.display = "block";
-    getElem("viewBox").remove();
-  };
-
-  return box;
-}
+// window.onload = function() {
+//   document.getElementById("loading").style.display = "none";
+// };
 
 function saveFn() {
   let inputData = {
@@ -45,24 +22,4 @@ function saveFn() {
   ipcRenderer.send("inputData", inputData);
   authorTA.value = "";
   quoteTA.value = "";
-}
-
-function viewFn() {
-  addBox.style.display = "none";
-  let box = createViewBox();
-  ipcRenderer.send("needViewData", null);
-
-  ipcRenderer.on("viewData", (event, jsonObj) => {
-    jsonObj.forEach(elem => {
-      var newlabel = document.createElement("Label");
-      newlabel.innerHTML = elem.QUOTE;
-      newlabel.style.width = "80%";
-      newlabel.style.marginLeft = "10%";
-
-      newlabel.setAttribute("class", "list-group-item");
-      box.appendChild(newlabel);
-    });
-  });
-  let bxc = getElem("boxContainer");
-  bxc.appendChild(box);
 }
