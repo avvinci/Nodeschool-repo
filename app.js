c6301be0a38a81d57714c95cd2e36ed643515ce6
avvinci <vinciabhinav7@gmail.com>
2019-07-23 14:18:25 +0530

    tray click ok

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-var electron = require("electron");
-const { app, Menu, Tray } = require("electron");
-const trayWindow = require("electron-tray-window");
-const { ipcMain } = require("electron");
+const { BrowserWindow, app, Menu, Tray, ipcMain } = require("electron");
+// const trayWindow = require("electron-tray-window");
 const csvFilePath = "./quotes.csv";
 
 let tray = null;
@@ -11,47 +9,19 @@ app.on("ready", () => {
     {
       label: "Add Quote",
       click: () => {
-        var newWindow = new electron.BrowserWindow({
-          webPreferences: {
-            nodeIntegration: true
-          },
-          width: 800,
-          height: 600
-        });
-        newWindow.loadURL(`file://${__dirname}/index.html`);
+        createMainWindow();
       }
     },
-    { label: "Item2", type: "radio" },
-    { label: "Item3", type: "radio", checked: true },
-    { label: "Item4", type: "radio" }
+    { label: "ViewQuotes" },
+    { label: "Settings" },
+    { label: "Misc" }
   ]);
   tray.setToolTip("Quotes collector.");
   tray.setContextMenu(contextMenu);
-  // });
-  // var mytrayWindow = new electron.BrowserWindow({
-  //   webPreferences: {
-  //     nodeIntegration: true
-  //   },
-  //   width: 400,
-  //   height: 400,
-  //   margin_x: 10,
-  //   margin_y: 10
-  // });
-
-  // // mytrayWindow.setMenu(null);
-  // mytrayWindow.loadURL(`file://${__dirname}/tray.html`);
-  // trayWindow.setOptions({
-  //   trayIconPath: "./Quotes.ico",
-  //   window: mytrayWindow
-  //   // windowUrl: `file://${__dirname}/tray.html`,
-  // });
-
-  // trayWindow.setWindowSize({
-  //   width: 400,
-  //   height: 400,
-  //   margin_x: 10,
-  //   margin_y: 10
-  // });
+  tray.on("click", () => {
+    console.log("tray clicked");
+    createTrayWindow();
+  });
 });
 
 ipcMain.on("needViewData", async event => {
@@ -84,3 +54,33 @@ ipcMain.on("inputData", async (event, inputData) => {
       console.log("...Done");
     });
 });
+
+function createMainWindow() {
+  var newWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    width: 800,
+    height: 600
+  });
+  newWindow.loadURL(`file://${__dirname}/index.html`);
+}
+
+function createTrayWindow() {
+  var mytrayWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    width: 400,
+    height: 400,
+    margin_x: 10,
+    margin_y: 10
+  });
+  mytrayWindow.setMenu(null);
+  mytrayWindow.loadURL(`file://${__dirname}/index.html`);
+  // trayWindow.setOptions({
+  // trayIconPath: "./Quotes.ico",
+  //   window: mytrayWindow
+  //   // windowUrl: `file://${__dirname}/tray.html`,
+  // });
+}
--- tray.js
+++ tray.js
@@ -1,18 +1,18 @@
-var electron = require("electron");
-const BrowserWindow = electron.remote.BrowserWindow;
-var b = document.getElementById("viewMainWindow");
+// var electron = require("electron");
+// const BrowserWindow = electron.remote.BrowserWindow;
+// var b = document.getElementById("viewMainWindow");
 
-b.onclick = () => {
-  createMainWindow();
-};
+// b.onclick = () => {
+//   createMainWindow();
+// };
 
-function createMainWindow() {
-  var newWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true
-    },
-    width: 800,
-    height: 600
-  });
-  newWindow.loadURL(`file://${__dirname}/index.html`);
-}
+// function createMainWindow() {
+//   var newWindow = new BrowserWindow({
+//     webPreferences: {
+//       nodeIntegration: true
+//     },
+//     width: 800,
+//     height: 600
+//   });
+//   newWindow.loadURL(`file://${__dirname}/index.html`);
+// }
