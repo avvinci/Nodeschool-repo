e139a7db6bf11f817f85a2565cbc10d90df18406
avvinci <vinciabhinav7@gmail.com>
2019-07-23 09:19:58 +0530

    tray working

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
 var electron = require("electron");
+const { app, Menu, Tray } = require("electron");
 const trayWindow = require("electron-tray-window");
 const { ipcMain } = require("electron");
 const csvFilePath = "./quotes.csv";
 
-electron.app.on("ready", function() {
-  var mytrayWindow = new electron.BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true
+let tray = null;
+app.on("ready", () => {
+  tray = new Tray("./Quotes.ico");
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Add Quote",
+      click: () => {
+        var newWindow = new electron.BrowserWindow({
+          webPreferences: {
+            nodeIntegration: true
+          },
+          width: 800,
+          height: 600
+        });
+        newWindow.loadURL(`file://${__dirname}/index.html`);
+      }
     },
-    width: 100,
-    height: 100
-  });
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" }
+  ]);
+  tray.setToolTip("Quotes collector.");
+  tray.setContextMenu(contextMenu);
+  // });
+  // var mytrayWindow = new electron.BrowserWindow({
+  //   webPreferences: {
+  //     nodeIntegration: true
+  //   },
+  //   width: 400,
+  //   height: 400,
+  //   margin_x: 10,
+  //   margin_y: 10
+  // });
 
-  // mytrayWindow.setMenu(null);
-  mytrayWindow.loadURL(`file://${__dirname}/tray.html`);
-  trayWindow.setOptions({
-    trayIconPath: "./Quotes.ico",
-    window: mytrayWindow
-    // windowUrl: `file://${__dirname}/tray.html`,
-  });
+  // // mytrayWindow.setMenu(null);
+  // mytrayWindow.loadURL(`file://${__dirname}/tray.html`);
+  // trayWindow.setOptions({
+  //   trayIconPath: "./Quotes.ico",
+  //   window: mytrayWindow
+  //   // windowUrl: `file://${__dirname}/tray.html`,
+  // });
 
-  trayWindow.setWindowSize({
-    width: 400,
-    height: 400,
-    margin_x: 10,
-    margin_y: 10
-  });
+  // trayWindow.setWindowSize({
+  //   width: 400,
+  //   height: 400,
+  //   margin_x: 10,
+  //   margin_y: 10
+  // });
 });
 
 ipcMain.on("needViewData", async event => {
